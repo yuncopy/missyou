18eeaee65061d1121ecb853efc6c41af7d6022b0
jackin.chen <jackin.chen@huolala.cn>
2023-03-28 17:59:20 +0800

    学习项目-加入容器 和 注入对象

--- src/main/java/com/lin/missyou/api/v1/BannerController.java
+++ src/main/java/com/lin/missyou/api/v1/BannerController.java
@@ -5,8 +5,10 @@ package com.lin.missyou.api.v1;
 import com.lin.missyou.core.interceptors.ScopeLevel;
 import com.lin.missyou.exception.http.NotFoundException;
 import com.lin.missyou.model.Banner;
+import com.lin.missyou.sample.hero.Mary;
 import com.lin.missyou.service.BannerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/banner")
 @Validated
+//@Lazy
 public class BannerController {
 
 //    @Autowired
@@ -21,6 +24,9 @@ public class BannerController {
 
     @Autowired
     private BannerService bannerService;
+
+    @Autowired //属性注入
+    private Mary mary;
 
     @GetMapping("/name/{name}")
     public Banner getByName(@PathVariable String name){
@@ -30,6 +36,31 @@ public class BannerController {
         }
         return banner;
     }
+
+    /*set方法注入
+    @Autowired
+    public void setMary(Mary mary) {
+        this.mary = mary;
+    }
+     */
+
+   /*构造函数注入
+    @Autowired
+    public BannerController(Mary mary){
+        //构造函数注入
+        this.mary = mary; // 定义属性使用构造函数注入
+    }
+     */
+
+    @GetMapping("/test")
+    public String getTest(){
+        mary.q();
+        return "你好 集润";
+    }
+
+
+
+
 
 //    @PostMapping(value = "/test/{id1}")
 //    public PersonDTO test(@PathVariable(name="id1") @Range(min = 1,max = 10,message = "不能超过10噢") Integer id,
---
+++ src/main/java/com/lin/missyou/sample/hero/Mary.java
@@ -0,0 +1,29 @@
+package com.lin.missyou.sample.hero;
+
+import com.lin.missyou.sample.ISkill;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Lazy
+public class Mary {
+    public Mary() {
+        System.out.println("Hello,Mary");
+    }
+    public void q(){
+        System.out.println("Mary Q");
+    }
+
+    public void w(){
+        System.out.println("Mary W");
+    }
+
+    public void e(){
+        System.out.println("Mary E");
+    }
+
+    public void r(){
+        System.out.println("Mary R");
+    }
+
+}
