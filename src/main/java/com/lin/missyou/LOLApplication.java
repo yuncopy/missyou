d74809e94e4045f3c270876c981c0dc5d23ae07f
jackin.chen <jackin.chen@huolala.cn>
2023-04-17 11:06:41 +0800

    EnableLOLConfiguration

--- src/main/java/com/lin/missyou/LOLApplication.java
+++ src/main/java/com/lin/missyou/LOLApplication.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 //@ComponentScan // 第一种 导入IOC容器
 //@Import(HeroConfiguration.class) //第二种 导入IOC容器
 //@Import(LOLConfigurationSelector.class) // 第三种 导入IOC容器
-@EnableLOLConfiguration
+@EnableLOLConfiguration //第四种 导入IOC容器
 public class LOLApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context =
